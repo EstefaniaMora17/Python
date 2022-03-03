09c0041aa6d6819f1c2b2baf90abb2003d707ad7
Emora <estefaniamora1702@gmail.com}>
2022-03-03 07:51:54 -0500

    ciclo for

---
+++ cicloFor.py
@@ -0,0 +1,11 @@
+'''for i in range(1,20,5):
+    print(i)'''
+
+#utilizamos for TRABAJA CON ARREGLOS (Listas)
+
+nombres=["juan","carlos","marta"]
+print(nombres)
+
+#for para RECORRER listas en python
+for nombre in nombres:
+    print (nombre)
